632b5ecc75267a28874f020076279623b31912e4
Chaemin <slaoc0115@gamil.com>
2023-05-18 09:04:24 +0900

    로그인 기능 추가 1) /login/login으로 GET요청이 오면, loginForm.jsp를 보여준다. 2) /login/login으로 POST요청이 오면, id와 비밀번호를 확인해서 일치하면 session에 id를 저장하고, userInfo.jsp로 redirect한다.(userInfo.jsp에서는 사용자가 입력한 id와 비밀번호를 보여준다.)  그리고 메뉴에서 Login을 Logout으로 변경한다. 3) 메뉴에서 Logout을 누르면, session을 종료하고 홈으로 이동한다.

--- src/main/java/com/spring/study/controller/LoginController.java
+++ src/main/java/com/spring/study/controller/LoginController.java
@@ -1,66 +1,68 @@
 package com.spring.study.controller;
 
+import com.spring.study.dao.UserDao;
 import com.spring.study.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+   @Autowired
+    UserDao userDao;
+
     @GetMapping("/login")
     public String loginView() {
-       return "login";
+        return "login";
     }
 
     /*
-    * 로그인하는 메서드
-    * 메서드명 : login
-    * 반환타입 : String
-    * */
+     * 로그인하는 메서드
+     * 메서드명 : login
+     * 반환타입 : String
+     * */
     @PostMapping("/login")
-    public String login(User user, boolean remember, String toURL, String active, Model model, HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("active = " + active);
-        // id저장 체크박스에 체크를 하고 로그인을 하면
-        // 1. 재로그인 시 id 저장 checkbox에 check가 되어있어야 한다.
-        // 2. id가 저장되어 있어야 한다.
+    public String login(User user, boolean remember, String toURL, Model model, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String userId = user.getId();
+            User loginCheckUser = userDao.selectUser(userId);
 
-        System.out.println("remember = " + remember);
-        String userID = user.getId();
+            // 1. 아이디와 비밀번호가 일치하는지 체크한다.
+            if (!loginCheck(user, loginCheckUser)) {
+                // 1-1. 일치하지 않으면 다시 login.jsp로 간다.
+                return "redirect:/login";
+            }
 
-        // 1. 아이디와 비밀번호가 일치하는지 체크한다.
-        if (!loginCheck(userID, user.getPwd())) {
-            // 1-1. 일치하지 않으면 다시 login.jsp로 간다.
-            return "redirect:/login";
+            // id를 session에 담는다.
+            HttpSession session = request.getSession();
+            session.setAttribute("id", userId);
+
+            // 1. remember가 true인지 체크한다.
+            if (remember) {
+                // 1-1. true이면 쿠키를 생성해서 쿠키에 id를 저장한다.
+                createCookie("id", userId, response);
+            } else {
+                // 1-2. false면 쿠키를 삭제한다.
+                removeCookie("id", "", response);
+            }
+            // 1-2. 일치하면 index.jsp로 가고
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        // id를 session에 담는다.
-        HttpSession session = request.getSession();
-        session.setAttribute("id", userID);
-
-        // 1. remember가 true인지 체크한다.
-        if (remember) {
-            // 1-1. true이면 쿠키를 생성해서 쿠키에 id를 저장한다.
-            createCookie("id", userID, response);
-        } else {
-            // 1-2. false면 쿠키를 삭제한다.
-            removeCookie("id", "", response);
-        }
-
-
         // 만약 로그인이 안 되어있는 상태에서 Board 게시판에 접근하려고하면, 로그인 페이지로 redirect하고
         // 거기서 로그인에 성공했을 경우에 index.jsp가 아닌 board.jsp를 보여줘야 한다.
         String url = toURL == null || toURL.equals("") ? "/" : toURL;
-
-        // 1-2. 일치하면 index.jsp로 가고
+//        String url = Objects.requireNonNullElse(toURL, "");
         return "redirect:" + url;
     }
 
@@ -84,8 +86,8 @@ public class LoginController {
         response.addCookie(cookie); // 응답에 쿠키 추가
     }
 
-    private boolean loginCheck(String id, String pwd) {
-        return "asdf".equals(id) && "1234".equals(pwd);
+    private boolean loginCheck(User user, User loginCheckUser) {
+        return user.getId().equals(loginCheckUser.getId()) && user.getPwd().equals(loginCheckUser.getPwd());
     }
 
 }
